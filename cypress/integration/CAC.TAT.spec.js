70659e5721ca820957e6b93a9f915e1da941178c
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-04-12 09:18:39 -0300

    test title

---
+++ cypress/integration/CAC.TAT.spec.js
@@ -0,0 +1,20 @@
+// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function(){
+        cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Luana')
+        cy.get('#lastName').type('DG')
+        cy.get('#email').type('luana@gmail.com')
+        cy.get('#open-text-area').type('testandoo')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+  })
